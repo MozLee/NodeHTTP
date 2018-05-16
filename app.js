14967b6bc400a73a61a012cf8a05c58452ad1c39
mozlee <lixin@mozlee.com>
2018-05-16 14:50:47 +0800

    1

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+let app = express();
+app.use(express.static(__dirname+'/public'))
+app.listen(8080,function () {
+	// body...q
+	console.log('xiaobaosystem启动成功')
+});
+../XiaoBaoSystem/public/avatar
